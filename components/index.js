174c07177dddc340c40ba104095cce516063d4ca
Ashandi Leonadi <ashandileonadi2001@gmail.com>
2023-03-14 18:09:43 +0700

    slicing detail page

--- components/index.js
+++ components/index.js
@@ -2,5 +2,16 @@ import HomeHeader from "./HomeHeader";
 import FocusedStatusBar from "./FocusedStatusBar";
 import NFTCard from "./NFTCard";
 import { CircleButton } from "./Button";
+import DetailsDesc from "./DetailsDesc";
+import DetailsBid from "./DetailsBid";
+import { SubInfo } from "./SubInfo";
 
-export { HomeHeader, FocusedStatusBar, NFTCard, CircleButton };
+export {
+  HomeHeader,
+  FocusedStatusBar,
+  NFTCard,
+  CircleButton,
+  DetailsDesc,
+  DetailsBid,
+  SubInfo,
+};
--- screens/Details.js
+++ screens/Details.js
@@ -1,13 +1,97 @@
-import { useNavigation } from "@react-navigation/native";
-import { Button, View } from "react-native";
+import { Text, View, SafeAreaView, Image, StatusBar } from "react-native";
+import {
+  DetailsBid,
+  DetailsDesc,
+  FocusedStatusBar,
+  SubInfo,
+} from "../components";
+import { CircleButton, RectButton } from "../components/Button";
+import { COLORS, FONTS, SHADOWS, SIZES, assets } from "../constants";
+import { FlatList } from "react-native-gesture-handler";
+import { Fragment } from "react";
 
-const Details = () => {
-  const navigation = useNavigation();
+const DetailsHeader = ({ data, navigation }) => {
+  return (
+    <View style={{ width: "100%", height: 373 }}>
+      <Image
+        source={data.image}
+        resizeMode="cover"
+        style={{ width: "100%", height: "100%" }}
+      />
+
+      <CircleButton
+        imgUrl={assets.left}
+        handlePress={() => navigation.goBack()}
+        left={15}
+        top={StatusBar.currentHeight + 10}
+      />
+      <CircleButton
+        imgUrl={assets.heart}
+        right={15}
+        top={StatusBar.currentHeight + 10}
+      />
+    </View>
+  );
+};
+
+const Details = ({ route, navigation }) => {
+  const { data } = route.params;
+  console.log("data", data);
 
   return (
-    <View style={{ padding: 50 }}>
-      <Button title="Back" onPress={() => navigation.navigate("Home")} />
-    </View>
+    <SafeAreaView>
+      <FocusedStatusBar
+        barStyle="dark-content"
+        backgroundColor="transparent"
+        transLucent
+      />
+
+      <View
+        style={{
+          width: "100%",
+          position: "absolute",
+          bottom: 0,
+          paddingVertical: SIZES.font,
+          justifyContent: "center",
+          alignItems: "center",
+          backgroundColor: "rgba(255,255,255,0.5)",
+          zIndex: 1,
+        }}
+      >
+        <RectButton minWidth={170} fontSize={SIZES.large} {...SHADOWS.dark} />
+      </View>
+
+      <FlatList
+        data={data.bids}
+        renderItem={({ item }) => <DetailsBid bid={item} />}
+        keyExtractor={(item) => item.id}
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={{
+          paddingBottom: SIZES.extraLarge * 3,
+        }}
+        ListHeaderComponent={() => (
+          <Fragment>
+            <DetailsHeader data={data} navigation={navigation} />
+            <SubInfo />
+            <View style={{ padding: SIZES.font }}>
+              <DetailsDesc data={data} />
+
+              {data.bids.length > 0 && (
+                <Text
+                  style={{
+                    fontSize: SIZES.font,
+                    fontFamily: FONTS.semiBold,
+                    color: COLORS.primary,
+                  }}
+                >
+                  Current Bid
+                </Text>
+              )}
+            </View>
+          </Fragment>
+        )}
+      />
+    </SafeAreaView>
   );
 };
 
